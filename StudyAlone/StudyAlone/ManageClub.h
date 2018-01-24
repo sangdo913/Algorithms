202bdc4b07f80f995ddda53685b66bf8f94d3486
SD-PC\SD <sangdo913@naver.com>
2018-01-24 18:33:05 +0900

    ManageClub Comments Added

--- StudyAlone/StudyAlone/ManageClub.h
+++ StudyAlone/StudyAlone/ManageClub.h
@@ -18,10 +18,12 @@ void ManageClub()
 	int T;
 	long long prime = 1000000007;
 	cin >> T;
-	const int A = 1;
-	const int B = 1 << 1;
-	const int C = 1 << 2;
-	const int D = 1 << 3;
+	//비트 마스킹
+	const int A = 1; // A : 0비트
+	const int B = 1 << 1; // B : 1비트
+	const int C = 1 << 2; // C : 2비트
+	const int D = 1 << 3; // D : 3비트
+
 	for (int tc = 1; tc <= T; tc++)
 	{
 		long long result = 0;
@@ -30,17 +32,19 @@ void ManageClub()
 		char in[10001];
 		cin >> in;
 		int in_size = strlen(in);
+
+		//첫날에 열쇠는 A에게만 있다.
 		ManageDP[0][A] = 1;
 
 		//DP
 		for (int i = 0; i < in_size; i++)
 		{
 			int manage = 1 <<ManageClub_Conv(in[i]);
-			for (int j = 1; j < 16; j++)
+			for (int j = 1; j < 16; j++) // j : 어제 방문한 사람들
 			{
-				for (int k = 1; k < 16; k++)
+				for (int k = 1; k < 16; k++) // k :  오늘 방문하는 사람들
 				{
-					if (!(j&k) || !(k&manage)) continue;
+					if (!(j&k) || !(k&manage)) continue; // 어제방문한 사람이 오늘 방문하지 않거나, 오늘 방문하는 사람중에 관리자가 없다.
 					ManageDP[i + 1][k] = (ManageDP[i+1][k] + ManageDP[i][j])% 1000000007;
 				}
 			}
