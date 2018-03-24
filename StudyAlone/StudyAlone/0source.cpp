ce41f8a5979711a27162e06782d2125cc1a5e901
sangdo913 <sangdo913@naver.com>
2018-03-25 01:01:32 +0900

    init2

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"GetOuter.h"
-
+#include<iostream>
+using namespace std;
 
 int main()
 {
 	freopen("0Text.txt","r",stdin);
+	
 	GetOuter();
 	return 0;
 }
