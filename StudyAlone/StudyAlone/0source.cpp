2ec5b504cd11047303bcbd8afe95650b0531a102
SangDo Lee <sangdolee@SangDoui-MacBook-Pro.local>
2018-05-04 22:08:11 +0900

    simple insu was solved

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include"Decompression.h"
+#include"SimpleInSu.h"
 
 using namespace std;
 
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/00Text.txt", "r", stdin);
-	Decompression();
+	SimpleInSu();
 	return 0;
 }
