43d1ccc38af6b948ae0dd8be99aefa9db2f1540a
SangDo Lee <sangdolee@SangDoui-MacBook-Pro.local>
2018-05-04 23:29:38 +0900

    New Can't Sleep Remmedy was solved

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include"Base64Decode.h"
+#include"NewCantSleepRemedy.h"
 
 using namespace std;
 
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/00Text.txt", "r", stdin);
-	Base64Decode();
+	NewCantSleepRemedu();
 	return 0;
 }
