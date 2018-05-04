db4fb50e6d6b19ca00a5a056660823b8b5720e74
SangDo Lee <sangdolee@SangDoui-MacBook-Pro.local>
2018-05-04 20:28:09 +0900

    TwoNumArr was solved

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include"ClockWiseNum.h"
+#include"TwoNumArr.h"
 
 using namespace std;
 
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/00Text.txt", "r", stdin);
-	ClockWiseNum();
+	TwoNumArr();
 	return 0;
 }
