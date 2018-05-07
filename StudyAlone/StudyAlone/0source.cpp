a23f7e9a96b1f719d1d1ef92ed19ff64db071c7c
SangDo Lee <>
2018-05-07 19:55:44 +0900

    Magnetic was solved

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include"GNS_Level3.h"
+#include"Magnetic.h"
 
 using namespace std;
 
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/00Text.txt", "r", stdin);
-	GNS_Level3();
+	Magnetic();
 	return 0;
 }
