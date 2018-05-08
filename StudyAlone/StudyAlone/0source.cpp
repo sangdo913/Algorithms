5fd794f6b86f78a17ee84a2950c6f95bbf487388
SangDo Lee <>
2018-05-08 13:14:26 +0900

    Arr max Sum and Flatten were solved

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include"KMP.h"
+#include"flatten.h"
 
 using namespace std;
 
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/00Text.txt", "r", stdin);
-	KMP();
+	Flatten();
 	return 0;
 }
