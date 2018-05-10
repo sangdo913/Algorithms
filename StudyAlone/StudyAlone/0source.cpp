08060463888fafca34d8dcf567194c0c435009b9
SangDo Lee <>
2018-05-10 10:50:51 +0900

    make brakets pair was solved

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include"Ladder2.h"
+#include"MakePair_GwalHo.h"
 
 using namespace std;
 
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/00Text.txt", "r", stdin);
-	ladder2();
+	MakePair_GwalHo();
 	return 0;
 }
