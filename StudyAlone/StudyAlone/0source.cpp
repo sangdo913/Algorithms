0fc69053edbfdf935906b68f73041509267ed2d6
SangDo Lee <>
2018-05-13 13:07:07 +0900

    Work order was solved

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include"longestCommonSubstring.h"
+#include"WorkOrder.h"
 
 using namespace std;
 
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/00Text.txt", "r", stdin);
-	longestCommonSubstring();
+	WorkOrder();
 	return 0;
 }
