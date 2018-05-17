76650f46e93341e55e0cfc31bb2778061208ae86
SangDo Lee <>
2018-05-17 11:21:44 +0900

    People network was revised

--- StudyAlone/StudyAlone/PeopleNetwork2.h
+++ StudyAlone/StudyAlone/PeopleNetwork2.h
@@ -1,61 +1,58 @@
 #include<cstdio>
+ int map[1000][1000], size;
 
-struct NetworkInfo
-{
-    int map[1000][1000], size;
-
-    void init()
-    {
-        scanf("%d ", &size);
-        for(int i = 0; i < size; i++)
-        {
-            for(int j = 0; j < size; j++)
-            {
-                scanf("%d ", &map[i][j]);
-                if(map[i][j] == 0) map[i][j] = 100000;
-            }
-        }
-    }
-
-    int getMin(int i1, int i2)
-    {
-        return i1 < i2 ? i1 : i2;
-    }
-        
-    int getRes()
-    {
-        for(int k = 0; k < size; k++)
-        {
-            for(int i = 0; i < size; i++)
-            {
-                for(int j = 0; j < size; j++)
-                {
-                    map[i][j] = getMin(map[i][k] + map[k][j], map[i][j]);
-                }
-            }
-        }
-        int res[1000] = {0};
-        int min = 987654321;
-        for(int i = 0; i < size; i++)
-        {
-            for(int j = 0; j < size; j++)
-            {
-                if(i != j) res[i] += map[i][j];
-            }
-            min = getMin(min, res[i]);
-        }
-        return min;
-    }
-} info;
-
-int PeopleNetwork2()
+int main()
 {
     int t;
     scanf("%d\n", &t);
-    for(int tc = 1; tc <= t; tc++)
+    for( int tc = 1; tc <= t; tc++)
     {
-        info.init();
-        printf("#%d %d\n", tc, info.getRes());
+        scanf("%d ", &size);
+        for( int i = 0; i < size; i++)
+        {
+            for( int j = 0; j < size; j++)
+            {
+                scanf("%d ", &map[i][j]);
+                if(map[i][j] == 0) map[i][j] = 987654321;
+            }
+        }
+        
+        
+         int sum;
+        for( int k = 0; k < size; k++)
+        {
+            for( int i = 0; i < size; i++)
+            {
+                if(i == k) continue;
+                for( int j = i + 1; j < size; j++)
+                {
+                    sum = map[i][k] + map[k][j];
+                    if(sum < map[i][j])
+                    {   
+                        map[j][i] = map[i][j] = sum;
+                    }
+                }
+            }
+        }
+        
+        for( int i = 0; i < size; i++)
+        {
+            map[i][i] = 0;
+        }
+         int min = 987654321;
+         int res;
+        
+        for( int i = 0; i < size; i++)
+        {
+            res = 0;
+            for( int j = 0; j < size; j++)
+            {
+                res += map[i][j];
+            }
+            if(min > res) min = res;
+        }
+        
+        printf("#%d %d\n", tc, min);
     }
     return 0;
 }
