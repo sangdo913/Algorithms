61cc44da2bdaec6a11fc6c36a18a4b40c27a4400
SangDo Lee <>
2018-07-13 22:31:42 +0900

    Hike and Swim were solved

--- StudyAlone/AlgorithmStudy/소스.cpp
+++ StudyAlone/AlgorithmStudy/소스.cpp
@@ -1,7 +1,14 @@
-#include"LongestPath.h"
+#include <stdio.h>
+#include<iostream>
+#include <stdlib.h>
+#include"Hike.h"
+
+using namespace std;
 
 int main()
 {
-	LongestPath();
+	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/AlgorithmStudy/Text.txt", "r", stdin);
+	Hike();
+	
 	return 0;
 }
