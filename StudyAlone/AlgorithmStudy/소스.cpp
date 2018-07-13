ae944ec2a7581fb59d29b856a04860f1f9dca5ff
SD-PC\SD <sangdo913@naver.com>
2018-07-13 23:26:56 +0900

    card2 and jocepus problem were added.

--- StudyAlone/AlgorithmStudy/소스.cpp
+++ StudyAlone/AlgorithmStudy/소스.cpp
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include"Hike.h"
+#include"LinkedList.h"
 
 using namespace std;
 
 int main()
 {
-	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/AlgorithmStudy/Text.txt", "r", stdin);
-	Hike();
+	//freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/AlgorithmStudy/Text.txt", "r", stdin);
+	//Hike();
 	
+	List li;
+	int n;
+	scanf("%d\n", &n);
+
+	while (n--) {
+		int in;
+		scanf("%d \n", &in);
+		li.insert(in);
+	}
+
+	li.setCursor();
+	int in;
+	while ((in = li.next()) != -1) {
+		printf("%d\n", in);
+	}
+
 	return 0;
 }
