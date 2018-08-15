02b7d843027743250d9c96eafd513c431df170ba
SD-PC\SD <sangdo913@naver.com>
2018-08-15 10:48:54 +0900

    code battle end

--- StudyAlone/OTherPeopleCode/소스.cpp
+++ StudyAlone/OTherPeopleCode/소스.cpp
@@ -4,91 +4,183 @@
 
 using namespace std;
 
-int map[50][50];
-const int dr[4] = { -1,1,0,0 };
-
-int n, m;
-const int dc[4] = { 0,0,-1,1 };
-const int change[4][4] = { { 2,1,3,0 },{ 0,2,1,3 },{ 3,0,2,1 },{ 1,3,0,1 } };
-const int UP = 0;
-const int RIGHT = 1;
-const int DOWN = 2;
-const int LEFT = 3;
-struct Cleaner
+struct TetInfo
 {
-	int direct;
-	int r, c;
-	int clean;
-	int check()
+	int blocks[5][8][16] =
 	{
-		int nextR = r + dr[change[direct][0]];
-		int nextC = c + dc[change[direct][0]];
-
-		bool dirty = false;
-
-		for (int i = 0; i < 4; i++)
 		{
-			nextR = r + dr[i];
-			nextC = c + dc[i];
+			{
+				1,1,1,1,
+				0,0,0,0,
+				0,0,0,0,
+				0,0,0,0
+			},
+			{
+				1,0,0,0,
+				1,0,0,0,
+				1,0,0,0,
+				1,0,0,0
+			}
+		},
 
-			dirty |= map[nextR][nextC] == 0;
-		}
-
-		if (!dirty)
 		{
-			nextR = r + dr[change[direct][1]];
-			nextC = c + dc[change[direct][1]];
-			if (map[nextR][nextC] == 1) return -1;
-			r = nextR; c = nextC;
-			return 2;
-		}
+			{
+				1,1,0,0,
+				1,1,0,0,
+				0,0,0,0,
+				0,0,0,0
+			}
+		},
 
-		nextR = r + dr[change[direct][0]];
-		nextC = c + dc[change[direct][0]];
-
-		if (map[nextR][nextC] == 0)
 		{
-			r = nextR;
-			c = nextC;
-			direct = (direct + 3) % 4;
-			return 1;
+			{
+				1,0,0,0,
+				1,0,0,0,
+				1,1,0,0,
+				0,0,0,0
+			},
+			{
+				1,1,1,0,
+				1,0,0,0,
+				0,0,0,0,
+				0,0,0,0
+			},
+			{
+				1,1,0,0,
+				0,1,0,0,
+				0,1,0,0,
+				0,0,0,0
+			},
+			{
+				0,0,1,0,
+				1,1,1,0,
+				0,0,0,0,
+				0,0,0,0
+			},
+			{
+				0,1,0,0,
+				0,1,0,0,
+				1,1,0,0,
+				0,0,0,0
+			},
+			{
+				1,0,0,0,
+				1,1,1,0,
+				0,0,0,0,
+				0,0,0,0
+			},
+			{
+				1,1,0,0,
+				1,0,0,0,
+				1,0,0,0,
+				0,0,0,0
+			},
+			{
+				1,1,1,0,
+				0,0,1,0,
+				0,0,0,0,
+				0,0,0,0
+			}
+		},
+		{
+			{
+				1,0,0,0,
+				1,1,0,0,
+				0,1,0,0,
+				0,0,0,0
+			},
+			{
+				0,1,1,0,
+				1,1,0,0,
+				0,0,0,0,
+				0,0,0,0
+			},
+			{
+				0,1,0,0,
+				1,1,0,0,
+				1,0,0,0,
+				0,0,0,0,
+			},
+			{
+				1,1,0,0,
+				0,1,1,0,
+				0,0,0,0,
+				0,0,0,0
+			}
+		},
+		{
+			{
+				0,1,0,0,
+				1,1,0,0,
+				0,1,0,0,
+				0,0,0,0
+			},
+			{
+				1,1,1,0,
+				0,1,0,0,
+				0,0,0,0,
+				0,0,0,0
+			},
+			{
+				0,1,0,0,
+				1,1,1,0,
+				0,0,0,0,
+				0,0,0,0
+			},
+			{
+				1,0,0,0,
+				1,1,0,0,
+				1,0,0,0,
+				0,0,0,0
+			}
 		}
+	};
+	int blockNum[5] = { 2,1,8,4,4 };
+	int map[503][503];
 
-		direct = (direct + 3) % 4;
-		return 2;
-	}
-	int process()
+	int n, m;
+	void init()
 	{
-		while (true)
+		memset(map, 0, sizeof(map));
+		scanf("%d %d\n", &n, &m);
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < m; j++)
+				scanf("%d", &map[i][j]);
+	}
+
+	int getMax()
+	{
+		int max = -1;
+		for (int r = 0; r < n; r++)
 		{
-			if (map[r][c] == 0)
+			for (int c = 0; c < m; c++)
 			{
-				map[r][c] = 2;
-				clean++;
-			}
-
-
-			while (true)
-			{
-				int flag = check();
-				if (flag == 1) break;
-				if (flag == -1) return clean;
-
+				for (int i = 0; i < 5; i++)
+				{
+					for (int j = 0; j < blockNum[i]; j++)
+					{
+						int sum = 0;
+						for (int row = 0; row < 4; row++)
+						{
+							for (int col = 0; col < 4; col++)
+							{
+								sum = blocks[i][j][row * 4 + col] ? sum + map[r + row][c + col] : sum;
+							}
+						}
+						max = max < sum ? sum : max;
+					}
+				}
 			}
 		}
+
+		return max;
 	}
-} cleaner;
+} info;
+
 int main()
 {
-	scanf("%d %d\n", &n, &m);
-	scanf("%d %d %d\n", &cleaner.r, &cleaner.c, &cleaner.direct);
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < m; j++)
-			scanf("%d", &map[i][j]);
-	}
-
-	int res = cleaner.process();
+	info.init();
+	int res = info.getMax();
 	printf("%d\n", res);
 	return 0;
 }
--- StudyAlone/StudyAlone/소스.cpp
+++ StudyAlone/StudyAlone/소스.cpp
@@ -1,9 +1,9 @@
-#include"BOJ14925.h"
+#include"BOJ1019.h"
 #include<cstdio>
 int main()
 {
 	//printf("%d\n", solution(2147483647));
-	BOJ14925();
+	BOJ1019();
 	return 0;
 }
 //2147483647
