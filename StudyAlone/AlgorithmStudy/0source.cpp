c04ea1086b6396e3caddc57685841f41efe11866
SangDo Lee <>
2018-09-06 17:42:08 +0900

    BOJ 2667 was solved

--- StudyAlone/AlgorithmStudy/0source.cpp
+++ StudyAlone/AlgorithmStudy/0source.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include "BOJ11559.h"
+#include "BOJ2667.h"
 #include<vector>
 
 using namespace std;
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/AlgorithmStudy/0Text.txt", "r", stdin);
-	BOJ11559();
+	BOJ2667();
 	return 0;
 }
