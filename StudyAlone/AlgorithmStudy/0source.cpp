b4cb8ca0c711a72366a6be65c7314a23214dd250
SangDo Lee <>
2018-10-01 20:13:11 +0900

    SWEA 4012 was solved

--- StudyAlone/AlgorithmStudy/0source.cpp
+++ StudyAlone/AlgorithmStudy/0source.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include<iostream>
 #include <stdlib.h>
-#include "BOJ2667.h"
+#include "SWEA4012.h"
 #include<vector>
 
 using namespace std;
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/AlgorithmStudy/0Text.txt", "r", stdin);
-	BOJ2667();
+	SWEA4012();
 	return 0;
 }
---
+++ StudyAlone/AlgorithmStudy/SWEA4012.h
@@ -0,0 +1,61 @@
+#include<iostream>
+
+using namespace std;
+
+int n;
+int info[16][16];
+
+int MIN(int i1, int i2){return i1 < i2 ? i1 : i2;}
+
+int res = 0x3f3f3f3f;
+
+int ABS(int i){return i > 0 ? i : -i;}
+
+void comb(int cnt, int len, int visit){
+    if(len == n) return;
+
+    if(n/2 == cnt){
+        int a = 0, b = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == j) continue;
+                if((visit & (1<<i)) && (visit & (1<<j))){
+                    a+= info[i][j];
+                                    
+                }
+                else if ((visit & (1 << i)) == 0 && ((visit &(1 << j)) == 0)){
+                    b+= info[i][j];
+                }
+            }
+        }
+        int sub = ABS(a - b);
+        res = MIN(sub,res);
+        return;
+    }
+
+    comb(cnt+1, len+1, visit | 1 << len);
+    comb(cnt, len+1, visit);
+}
+
+int SWEA4012(){
+    int t;
+    cin>> t;
+    for(int tc= 1; tc  <= t; tc++){
+        cin >> n;
+
+        res = 0x3f3f3f3f;
+
+        for(int i = 0; i <n;i++){
+            for(int j =0; j < n; j++){
+                cin >> info[i][j];
+            }
+        }
+
+        comb(0, 0, 0);
+
+        cout << '#' << tc << ' ' << res << '\n';
+    }
+
+    return 0;
+}
