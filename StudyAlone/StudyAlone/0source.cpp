2914e6371c3d28c5cfb9d5b4a769c8553e68fece
SangDo Lee <>
2019-02-15 17:21:53 +0900

    update

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,24 +1,13 @@
 
 #include <stdio.h>
-#include<iostream>
-#include <stdlib.h>
 #include"BOJ8982.h"
-#include<vector>
 
 using namespace std;
 
 int main()
 {
 	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/0Text.txt", "r", stdin);
-	//int a = colution(2147483647);
-	//printf("%d\n", a);
 	Do();
 	
-	//long long a;
-	//for(a = 1; a < 2147483647LL; a*=3){
-	/*	printf("%lld\n", a);
-	}
-	printf("%lld\n", a);
-*/
 	return 0;
 }
