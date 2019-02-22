5c1aa455c3a21df1fc1907de473eeb281ae1e422
SangDo Lee <>
2019-02-22 16:59:00 +0900

    BOJ16965 was sovled

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include"BOJ16965.h"
 
 using namespace std;
--- StudyAlone/StudyAlone/BOJ16965.h
+++ StudyAlone/StudyAlone/BOJ16965.h
@@ -1,78 +1,63 @@
 #include<iostream>
-#include<cstring>
+#include<queue>
 
 using namespace std;
 
-int node[101*4];
-int lazy[101*4], n;
-int line[2][101], idx;
+int line[2][101], idx = 1;
+int adj[101][101];
+int alen[101];
 
-#define MAX(x, y) ((x) > (y) ? (x) : (y))
+int visit[101], n, query = 1;
 
-void insert(int idx, int s, int e, int left, int right, int value){
-    node[idx] =  MAX(node[idx], lazy[idx]);
-    if(left != right) {
-        lazy[idx*2] = MAX(lazy[idx*2], node[idx*2]);
-        lazy[idx*2+1] = MAX(lazy[idx*2+1], node[idx*2+1]);
+bool bfs(int from, int to){
+    queue<int> que;
+    que.push(from);
+
+    while(que.size()){
+        int now = que.front(); que.pop();
+        if(now == to) return true;
+
+        for(int i = 0; i < alen[now]; i++){
+            int next = adj[now][i];
+
+            if(visit[next] == query) continue;
+            visit[next] = query;
+            que.push(next);
+        }
     }
 
-    if(right < s || e < left) return;
-
-    node[idx] = MAX(node[idx], value);
-
-    if(left != right) {
-        lazy[idx*2] = MAX(lazy[idx*2], node[idx]);
-        lazy[idx*2+1] = MAX(lazy[idx*2+1], node[idx]);
-    }
-
-    if(s <= left && right <= e){ return; }
-
-    int m = (left + right)/2;
-    insert(idx*2, s,e, left, m, value);
-    insert(idx*2+1, s, e, m+1, right, value);
+    return false;
 }
 
-int get(int idx, int s, int e, int left, int right){
-    node[idx] =  MAX(node[idx], lazy[idx]);
-
-    if(left != right) {
-        lazy[idx*2] = MAX(lazy[idx*2], node[idx]);
-        lazy[idx*2+1] = MAX(lazy[idx*2+1], node[idx]);
-    }
-
-    if(right < s || e < left){
-        return -0x3f3f3f3f;
-    }
-
-    if(s <= left && right <= e){
-        return node[idx];
-    }
-
-    int m = (left + right) /2;
-
-    return MAX(get(idx*2, s,e, left, m), get(idx*2+1, s,e, m+1, right));
-}
-
-int l = -1000000000;
-int r = 1000000000;
 int Do(){
-    ios_base::sync_with_stdio(0), cout.tie(0), cin.tie(0);
-
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     cin >> n;
-    while(n--){
-        int a, b, c;
-        cin >> a >> b  >> c;
 
-        if(a == 1){
-            insert(1, b,c, l, r, c);
+    while(n--){
+        int a, b,c;
+
+        cin >> a >> b >> c;
+
+        if(a ==1){
             line[0][idx] = b;
             line[1][idx] = c;
+
+            for(int i  = 1; i < idx; i++){
+                int x = line[0][i], y = line[1][i];
+
+                if((b < x && x < c) || (b < y && y < c)){
+                    adj[i][alen[i]++] = idx;
+                }
+                if((x < b && b < y) || (x < c && c < y)){
+                    adj[idx][alen[idx]++] = i;
+                }
+            }
+            idx++;
         }
 
         else{
-            int x1 = line[0][b], y1 = line[1][b], x2 = line[0][c], y2 = line[1][c];
-            int to = get(1, x1,y1, l,r);
-            if(x2 < to && to < y2);
+            cout << bfs(b,c) << '\n';
+            query++;
         }
     }
     return 0;
