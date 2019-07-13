3b1f2a1518a515f8250b0ae8f380fcafbafab478
SangDo Lee <>
2019-07-13 13:11:20 +0900

    kakuro2 was sovled by another way

--- StudyAlone/StudyAlone/KAKURO2.h
+++ StudyAlone/StudyAlone/KAKURO2.h
@@ -2,9 +2,10 @@
 
 using namespace std;
 
+int dp[10][46][1024];
 int n;
 bool map[20][20];
-int x[1000], y[1000], sum[1000], check[1000];
+int x[1000], y[1000], sum[1000];
 int len[1000];
 bool sero[1000];
 int arr[20][20];
@@ -13,68 +14,86 @@ int dr[2] = { 0,1 };
 int dc[2] = { 1,0 };
 int info[1000][1000];
 int mapinfo[20][20];
-int infolen[1000];
 int tc;
-int maxSum[10];
 
-int make(int idx, int ilen){
-	if(!idx && !len[idx]) return 0;
-	if(len[idx] && sum[idx] > maxSum[len[idx]]) return idx;
-	if(ilen == infolen[idx]){
-		if(sum[idx]) return -1;
-		int nid = 0;
+int getNum(int bit){
+	int res = 0;
+	while(bit){
+		res++;
+		bit -= bit & -bit;
+	}
+	return res;
+}
+int getSum(int bit){
+	int ret = 0;
+	for(int i = 1; i < 10; ++i){
+		if(bit & (1<<i)) ret += i;
+	}
+	return ret;
+}
+int avail[4000];
+int used[4000];
+
+int getAvail(int r, int c){
+	int id1 = mapinfo[r][c] >> 10, id2= mapinfo[r][c] & 1023;
+	return dp[len[id1]][sum[id1]][used[id1]] & dp[len[id2]][sum[id2]][used[id2]];
+}
+bool isfull(int idx){
+	return getNum(used[idx]) == len[idx];
+}
+bool isok(int idx){
+	return isfull(idx) || dp[len[idx]][sum[idx]][used[idx]];
+}
+
+bool make(int idx, int ilen){
+	if(ilen == len[idx]){
 		for(int i = 0; i < n; ++i) for(int j = 0; j < n; ++j){
 			if(!arr[i][j]) continue;
-			int temp;
-			int i1 = mapinfo[i][j] >>10;
-			int i2 = mapinfo[i][j] & 1023;
-			if(!len[i1] && !sum[i1]) i1 = 0;
-			if(!len[i2]&& !sum[i2]) i2 = 0;
-
-			if(len[i1] == len[i2]){
-				temp = sum[i1] < sum[i2] ? i1 : i2;
+			int id1 = mapinfo[i][j] >> 10;
+			int id2 = mapinfo[i][j] &1023;
+			int a1 = getNum(dp[len[id1]][sum[id1]][used[id1]]);
+			int a2 = getNum(dp[len[id2]][sum[id2]][used[id2]]);
+    
+			if(isfull(id1) && isfull(id2)) continue;
+			if(isfull(id1)){
+				return make(id2,0);
 			}
-			else if(len[i1] < len[i2]) temp = i1;
-			else temp = i2;
-			if(!len[temp] && !sum[temp]) continue;
-			if(len[nid] > len[temp]) nid = temp;
-			else if(len[nid] == len[temp]){
-				if(sum[nid] > sum[temp]) nid = temp;
+			if(isfull(id2)) 
+				return make(id1,0);
+
+			if(a1 == a2) {
+				if(sum[id1] < sum[id2]) 
+					if(isok(id1)) return make(id1, 0);
+					else return make(id2,0);
+			}
+			else{
+				if(len[id1] - a1 < len[id2] - a2) 
+					if(isok(id1)) return make(id1, 0);
+					else return make(id2, 0);
 			}
 		}
-		if(nid == 0) return 0;
-		int res = make(nid, 0);
-		return res == 0 ? 0 : -1;
+		return true;
 	}
-	int r = info[idx][ilen] >> 10, c = info[idx][ilen]&1023;
-	if(arr[r][c]){
-		return make(idx, ilen+1);
-	}
-	int idx2 = (mapinfo[r][c] >> (10 * !sero[idx])) & 1023;
+	if(!isok(idx)) return false;
+	int r,c,tb; 
+	r = info[idx][ilen] >> 10, c = info[idx][ilen] & 1023;
+	if(arr[r][c]) return make(idx, ilen+1);
+	int id2 = (mapinfo[r][c] >>(10*!sero[idx])) & 1023;
+	tb = getAvail(r,c);
 
+		for(int i = 1; i < 10; ++i){
+			if((1<<i) & tb){
+				used[idx] |= 1<<i;
+				used[id2] |= 1<<i;
+				arr[r][c] = i;
 
-	for(int num =1; num < 10; num++){
-		if(check[idx] & (1<<num)) continue;
-		if(check[idx2] & (1<<num)) continue;
-		if(sum[idx2] - num < 0) return -1;
-		if(sum[idx] - num < 0) return -1;
-		check[idx] += 1<<num;
-		check[idx2] += 1 << num;
-		len[idx]--, len[idx2]--;
-		arr[r][c] = num;
-		sum[idx]-= num;
-		sum[idx2] -= num;
-		int res = make(idx, ilen+1);
-		if(res == 0) return 0;
-		
-		arr[r][c] = 0;
-		check[idx] ^= 1 << num;
-		check[idx2] ^= 1<<num;
-		sum[idx2] += num;
-		sum[idx] += num;
-		len[idx]++, len[idx2]++;
-	}
-	return -1;
+				if(make(idx, ilen+1)) return true;
+				used[idx] ^= 1<<i;
+				used[id2] ^= 1 <<i;
+				arr[r][c] = 0;
+			}
+		}
+	return false;
 }
 
 int Do() {
@@ -82,51 +101,42 @@ int Do() {
 	cin >> tc;
 	++tc;
 
-	maxSum[0] = 1000;
-	maxSum[1] = 9;
-	int nnum = 8;
-	for(int i = 2; i < 10; ++i){
-		maxSum[i] = maxSum[i-1] + nnum--;
+	for(int set = 2; set < 1024; set+=2){
+		int l = getNum(set);
+		int ssum = getSum(set);
+		int subset = set;
+
+		while(subset){
+			dp[l][ssum][set&~subset]  |= set &~(set&~subset);
+			subset--;
+			subset = set& subset;
+		}
 	}
-	len[0] = 10000;
 
 	while (--tc) {
 		cin >> n;
-		for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) cin >> map[i][j], mapinfo[i][j] = arr[i][j] = 0 ;
+		for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) cin >> map[i][j], mapinfo[i][j] = arr[i][j] = 0;
 		cin >> m;
 
+		int ii = 0;
+		len[0] = 1000, sum[0] = 1000;
 		for (int i = 1; i <= m; ++i) {
-			check[i] = 0;
-			infolen[i] = 0;
+			used[i] = 0;
 			len[i] = 0;
 			cin >> y[i] >> x[i] >> sero[i] >> sum[i];
 			y[i] -= !sero[i], x[i] -= sero[i];
 			int r = y[i];
 			int c = x[i];
 			while (r < n && c < n && map[r][c]) {
-				len[i]++;
-				info[i][infolen[i]++] = (r << 10) + c;
+				info[i][len[i]++] = (r << 10) + c;
 				mapinfo[r][c] |= i << (10*(int)sero[i]);
 				r += dr[sero[i]], c += dc[sero[i]];
 			}
+			if(len[i] == len[ii]) ii = sum[i] < sum[ii] ? i : ii;
+			else ii = len[i] < len[ii] ? i : ii;
 		}
 
-		int cnt = m;
-		while (cnt--) {
-			int idx = 0;
-			for (int i = 1; i <= m; ++i) {
-				if (!len[i] && !sum[i]) continue;
-				if (len[idx] > len[i]) {
-					idx = i;
-				}
-				else if (len[idx] == len[i] && sum[idx] > sum[i]) {
-					idx = i;
-				}
-			}
-			if (idx) {
-				make(idx, 0);
-			}
-		}
+		for(int ii = 1; ii <=m; ++ii)make(ii,0);
 
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < n; ++j) cout << arr[i][j] << ' ';
