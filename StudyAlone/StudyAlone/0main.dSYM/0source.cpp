9fc6e723f7b8c974ef193af40210213082feee74
sangdo913 <sangdo913@naver.com>
2019-08-18 04:08:30 +0900

    windows setting in vs code

---
+++ StudyAlone/StudyAlone/0main.dSYM/0source.cpp
@@ -0,0 +1,16 @@
+#include<stdlib.h>
+#include <stdio.h>
+//#include"ARCTIC.h"
+
+using namespace std;
+
+int main()
+{
+	//freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/0Text.txt", "r", stdin);
+	freopen("C:/Users/SD/Desktop/Git/Algorithm/StudyAlone/StudyAlone/0Text.txt", "r", stdin);
+	//Do();
+
+	printf("Helloworld2\n");
+
+	return 0;
+}
--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -1,11 +1,15 @@
-#include <stdio.h>
+#include<stdlib.h>
+#include<stdio.h>
 #include"ARCTIC.h"
 
 using namespace std;
 
 int main()
 {
-	freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/0Text.txt", "r", stdin);
+	//freopen("/Users/sangdolee/Desktop/Algorithm/StudyAlone/StudyAlone/0Text.txt", "r", stdin); // mac
+	//freopen("C:\\Users\\SD\\Desktop\\Git\\Algorithms\\StudyAlone\\StudyAlone\\0Text.txt", "r", stdin); //window 10
+	freopen("0Text.txt", "r", stdin); 
+
 	Do();
 
 	return 0;
