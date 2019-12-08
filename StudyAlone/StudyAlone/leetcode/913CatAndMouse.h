e658d8f2c81dabd3e7fb9283ed68450b7d1e0b3e
sangdo913 <sangdo913@naver.com>
2019-12-08 20:45:02 +0900

    ii

---
+++ StudyAlone/StudyAlone/leetcode/913CatAndMouse.h
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+
+using namespace std;
+#define rint register int
+struct node{
+    int c,m;
+};
+int visit[50][50];
+int tc; 
+int candraw;
+
+class Solution {
+public:
+    int catMouseGame(vector<vector<int>>& graph) {
+        ++tc;
+
+        queue<node> Q; 
+        Q.push({1,2});
+        
+    }
+};
+
+int main(){
+    return 0;
+}
