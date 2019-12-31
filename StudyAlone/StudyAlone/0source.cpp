0206447b57c431297b0042e592d0ceb3aee4b9db
sangdo913 <sangdo913@naver.com>
2019-12-31 22:36:22 +0900

    dictionary was solved

--- StudyAlone/StudyAlone/0source.cpp
+++ StudyAlone/StudyAlone/0source.cpp
@@ -2,5 +2,5 @@
 #include<stdio.h>
 //#include"BOJ/BOJ17281.h"
 //#include"ARCTIC.h"
-#include"leetcode/968BinaryTreeCameras.h"
-//#include"AOJ/NH.h"
+//#include"leetcode/968BinaryTreeCameras.h"
+#include"AOJ/DICTIONARY.h"
