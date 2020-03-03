76f80deae0be6b68f16ce7d5dcd94481fd5c78b1
sangdo913 <sangdo913@naver.com>
2020-03-03 22:45:20 +0900

    serialize and deserialize fixed

--- StudyAlone/StudyAlone/leetcode/Serialize and Deserialize Binary Tree.h
+++ StudyAlone/StudyAlone/leetcode/Serialize and Deserialize Binary Tree.h
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 #include<string>
 using namespace std;
 
@@ -8,58 +9,64 @@ using namespace std;
       TreeNode *right;
       TreeNode(int x) : val(x), left(NULL), right(NULL) {}
   };
- 
-string tostr(int i){
-    string s; 
-    s.resize(4);
-    s[0] = i &0xff;
-    s[1] = (i>>8) & 0xff;
-    s[2] = (i>>16) & 0xff;
-    s[3] = (i >> 24) & 0xff;
-    return s;
-}
 
-#define LSFT(x,a) (((unsigned int)(x) << (a)))
-int toint(string a){
-    return (a[3]&0xff)  | LSFT(a[2], 8) | LSFT(a[1], 16) | LSFT(a[0],24); 
-}
+/**
+ * Definition for a binary tree node.
+* struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+#define LSFT(x,cur,a) (((unsigned int)((x)[cur]) << (a)))
+vector<TreeNode> nodes(10000, TreeNode(0));
+int nid = 0;
 
-TreeNode* mydeserialize(string &data) {
-    int val = toint(string(data.rbegin(), data.rbegin()+4));
-    TreeNode *nn = new TreeNode(val);
-    data.pop_back();
-    data.pop_back();
-    data.pop_back();
-    data.pop_back();
-    char next = data.back();
-    data.pop_back();
-    if(next == 'a' || next == 'l'){nn->left = mydeserialize(data); }
-    if(next == 'a' || next == 'r') nn->right = mydeserialize(data);
+TreeNode* mydeserialize(string &data, int &i) {
+    TreeNode *nn = &nodes[nid++];
+    nn->val = LSFT(data,i+3, 24) | LSFT(data,i+2,16) | LSFT(data, i+1, 8) | data[i]&0xff; 
+    i+=4;
+    char next = data[i++];
+    if(next == 'a' || next == 'l') nn->left = mydeserialize(data,i);
+    else nn->left = 0;
+    if(next == 'a' || next == 'r') nn->right = mydeserialize(data, i);
+    else nn->right = 0;
     return nn;
+}
+
+void myserialize(string &ret, TreeNode* root) {
+    if(!root) return;
+    int i = root->val;
+    ret.push_back(i &0xff);
+    ret.push_back((i>>8) & 0xff);
+    ret.push_back((i>>16) & 0xff);
+    ret.push_back((i >> 24) & 0xff);
+    
+    if(root->right && root->left) ret.push_back('a');
+    else if(!root->right && !root->left) ret.push_back('n');
+    else if(root->left) ret.push_back('l');
+    else ret.push_back('r');
+    if(root->left) myserialize(ret, root->left);
+    if(root->right) myserialize(ret, root->right);
 }
 
 class Codec {
 public:
-
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(!root) return string();
-        string ret = "";
-        if(root->right) ret += serialize(root->right); 
-        if(root->left) ret += serialize(root->left);
-        if(root->right && root->left) ret +=string("a");
-        else if(!root->right && !root->left) ret += string("n");
-        else if(root->left) ret += string("l");
-        else ret += string("r");
-        ret += tostr(root->val);
-        
-        return ret;
+        string str("");
+        str.reserve(10000);
+        myserialize(str, root);
+        return str;
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
         if(data == "") return 0;
-        return mydeserialize(data);
+        nid= 0;
+        int cur =0;
+        return mydeserialize(data,cur);
     }
 };
 
