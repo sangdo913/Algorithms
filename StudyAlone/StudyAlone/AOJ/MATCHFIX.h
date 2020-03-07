4226a07c56c1d11c7db7b4d911d30fd44ea8b9a0
sangdo913 <sangdo913@naver.com>
2020-03-07 12:49:55 +0900

    BISHOPS was solved

--- StudyAlone/StudyAlone/AOJ/MATCHFIX.h
+++ StudyAlone/StudyAlone/AOJ/MATCHFIX.h
@@ -2,6 +2,7 @@
 #include<queue>
 #include<cstring>
 #define MIN(x,y) ((x) > (y) ? (y) : (x))
+#define INF 0x3f3f3f3f
 
 using namespace std;
 
@@ -15,29 +16,28 @@ int win[12];
 int s, t;
 int n, m;
 int matchnum;
-int tc;
+int flow;
 
 bool networkflow(int dst){
-    memset(f,0,sizeof(f));
-    queue<int> Q;
     for(int i = 1; i < n; ++i){
-        if(win[i] > dst-1) return false;
+        if(dst -1 < win[i]) return false;
         c[i][t] = dst-win[i]-1;
         c[t][i] = 0;
     }
-    c[0][t] = 0;
+    c[0][t] = dst - win[0];
     c[t][0] = 0;
 
-    int flow = 0;
     while(true){
+        queue<int> Q;
         for(int i = 0; i <= t; ++i) parent[i] = -1;
         parent[s] = s;
         Q.push(s);
         while(!Q.empty()){
             int here = Q.front(); Q.pop();
             if(here == t) break;
-            for(int i = 0; i <alen[here]; ++i){
-                int next = adj[here][i];
+            for(int i = 0; i <= t; ++i){
+                int next = i;
+                if(next == here) continue;
                 if(parent[next] != -1) continue;
                 if(c[here][next] > f[here][next]){
                     parent[next] = here;
@@ -47,7 +47,7 @@ bool networkflow(int dst){
         }
         if(parent[t] == -1) break;
         int here = t;
-        int minflow = 0x3f3f3f3f;
+        int minflow = INF;
         while(here != s ){
            minflow = MIN(minflow, c[parent[here]][here] - f[parent[here]][here]); 
            here = parent[here];
@@ -60,8 +60,17 @@ bool networkflow(int dst){
         }
         flow += minflow;
     }
-    //cout <<dst << ' ' <<   flow << endl;
-    return flow >= matchnum-(dst-win[0]);
+    if(f[0][t] + win[0] != dst) return false;
+
+    return flow == m;
+}
+
+int bs(){
+    flow = 0;
+    for(int i = win[0]; i <= win[0] + m; ++i){
+        if(networkflow(i)) return i;
+    }
+    return -1;
 }
 
 int main(){
@@ -74,6 +83,7 @@ int main(){
     while(C--){
         cin >> n >> m;
         memset(c,0,sizeof(c));
+        memset(f,0,sizeof(f));
         s = n + m;
         t = s+1;
         matchnum = m;
@@ -86,20 +96,20 @@ int main(){
         for(int i = 0; i < m; ++i){
             int a, b;
             cin >> a >> b;
+            if(b < a){
+                a^=b^=a^=b;
+            }
             c[n+i][a] = 1;
             c[n+i][b] = 1;
             c[s][n+i] = 1;
-            adj[n+i][0] = a, adj[n+1][1] = b;
+            adj[n+i][0] = a, adj[n+i][1] = b;
             alen[n+i] = 2;
             adj[s][i] = n+i;
             c[s][n+i] = 1;
         }
         alen[s] = m;
-        
-        for(int i = 0; i <= 10; ++i) cout << networkflow(i) << ' ';
-            
-        cout << endl;
-
+        int res = bs();
+        cout << res << '\n';
     }
     return 0;
 }
