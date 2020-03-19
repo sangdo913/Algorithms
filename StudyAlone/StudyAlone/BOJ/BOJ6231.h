a9a589ade64ee9a053e25d8b1df532c5b66d6ac1
SD-PC\SD <sangdo913@naver.com>
2020-03-20 02:44:30 +0900

    boj 6231 was  solved(see answer)

--- StudyAlone/StudyAlone/BOJ/BOJ6231.h
+++ StudyAlone/StudyAlone/BOJ/BOJ6231.h
@@ -1,55 +1,59 @@
 #include<iostream>
+#include <algorithm>
 #define MOD 10007
-#include<algorithm>
 
 using namespace std;
 
-int rangesum[100001][30];
+pair<unsigned long long, int> myhash[100002];
+int num[30];
 int bits[MOD];
 
-bool parametric(int n){
-
-    return false;
-}
-
 int main(){
-    freopen("0Text.txt","r", stdin);
-    int n, k;
+    freopen("0Text.txt", "r", stdin);
+    int n,k;
+    
     cin >> n >> k;
-    for(int i = 0 ; i < k; ++i) {
-        bits[(1<<i)%MOD] = i;
+    for(int i = 0; i < k; ++i) bits[(1<<i)%MOD] = i;
+    
+    int x;
+    myhash[0] = {0,0};
+    for(int i = 1;i <= n;++i){
+        cin >> x;
+
+        while(x){
+            int b = x&-x;
+            num[bits[b%MOD]]++;
+            x -= b;
+        }
+        
+        int mmin = num[0];
+        
+        for(int j = 1;j < k;++j)
+            if(mmin > num[j]) mmin = num[j];
+        
+        for(int j = 0;j < k;++j)
+            num[j] -= mmin;
+        
+        unsigned long long key = 0;
+
+        for(int j = 0; j < k;++j) key = key * 100007 + num[j];
+        myhash[i] = {key, i};
     }
-
-    for(int i = 1; i <= n; ++i){
-        int num;
-        cin >> num; 
-        while(num){
-            int b = num & -num;
-            rangesum[i][bits[b%MOD]] = 1;
-            num -= b;
+    
+    sort(myhash, myhash + n+1);
+    
+    int res = 0;
+    for(int i = 0; i <= n; ){
+        int j = i;
+        while(j <= n && myhash[i].first == myhash[j].first) ++j;
+        int diff = myhash[j-1].second - myhash[i].second;
+        if(res < diff){
+            res = diff;
         }
-        for(int j = 0; j < k; ++j){
-            rangesum[i][j] += rangesum[i-1][j];
-        }
+        i = j;
     }
-
-    for(int i = 1; i <= n; ++i){
-        int l , r;
-        int min = 0x3f3f3f3f;
-        for(int j = 0; j < k; ++j){
-            int diff = rangesum[i-1][j] - rangesum[n][j];
-            if(min > diff) min = diff;
-        }
-        l = 0, r = min;
-
-        while(l <= r){
-            int m = (l+r)/2;
-            if(parametric(m)){
-                m = l+1;
-            }
-            else m = r-1;
-        }
-    }
-
+    
+    cout << res;
+    
     return 0;
 }
