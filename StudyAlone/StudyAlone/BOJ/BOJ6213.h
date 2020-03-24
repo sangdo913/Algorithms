f656ea0383db3cb2b137376b10631c27a31d81ed
sangdo913 <sangdo913@naver.com>
2020-03-24 22:20:20 +0900

    BOJ6213.h

--- StudyAlone/StudyAlone/BOJ/BOJ6213.h
+++ StudyAlone/StudyAlone/BOJ/BOJ6213.h
@@ -1,49 +1,8 @@
 #include<iostream>
-int nodes[2][200004];
-
-bool MIN(int x, int y){
-    return x < y;
-}
-bool MAX(int x, int y){
-    return x > y;
-}
-
-bool (*cmp[2])(int a, int b) = {MIN, MAX};
-int NONE[2] = {0x3f3f3f3f, -1};
-
-int inittree(int id, int nid, int v, int left, int right, int fid){
-    if(id < left||right < id) return NONE[fid];
-    if(left == right){
-        return nodes[fid][nid] = v;
-    }
-    int m = (left+right)/2;
-    int l = nid*2;
-    int r = l+1;
-    int lv = inittree(id, l, v, left, m, fid);
-    int rv = inittree(id, r, v, m+1, right, fid);
-
-    if(!cmp[fid](lv, rv)) {
-        lv = rv;
-    }
-    if(!cmp[fid](nodes[fid][nid], lv)){
-        nodes[fid][nid] = lv;
-    }
-    return nodes[fid][nid];
-}
-
-int getvalue(int id, int left, int right, int start, int end, int fid){
-    if(right < start || end < left) return NONE[fid];
-    if(left <= start && end <= right) return nodes[fid][id];
-    int l = id*2;
-    int r = l+1;
-    int m = (start+end)/2;
-    int lv = getvalue(l, left, right, start, m, fid);
-    int rv = getvalue(r, left, right, m+1, end, fid);
-    if(!cmp[fid](lv,rv)){
-        lv = rv;
-    }
-    return cmp[fid](lv,rv) ? lv : rv;
-}
+#define MAX(x,y) ((x) >(y) ? (x) : (y))
+#define MIN(x,y) ((x) > (y) ? (y) : (x))
+int mmax[200002];
+int mmin[200002];
 
 using namespace std;
 
@@ -52,28 +11,46 @@ int main(){
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, q;
     cin >> n >> q;
-    for(int i = 1; i <= 4*n; ++i) nodes[0][i] = 0x3f3f3f3f, nodes[1][i] = -1;
+    int bn = 1;
+    while(bn < n) bn <<=1; bn--;
+    for(int i = 1; i <= bn+n; ++i) mmax[i] = 0, mmin[i] = 0x3f3f3f3f;
+
     for(int i  = 1; i <= n; ++i){
         int x ; 
         cin >> x;
-        inittree(i,1, x, 1,n, 0);
-        inittree(i, 1, x, 1, n, 1);
+        for(int j = bn + i; j; j>>=1){
+            mmin[j] = MIN(mmin[j], x);
+            mmax[j] = MAX(mmax[j], x);
+        }
     }
 
-    // int cnt = 0;
-    // for(int i =1; cnt < n;){
-    //     for(int j = 0; j < (1<<(cnt)); ++j, ++i){
-    //         cout << nodes[1][i] << ' ';
-    //     }
-    //     cnt++;
-    //     cout << endl;
-    // }
     while(q--){
-        int f,t;
-        cin >>f >> t;
-        // int mmax = getvalue(1,f,t,1,n,1);
-        // int mmin = getvalue(1,f,t,1,n,0);
-        cout << getvalue(1, f,t, 1, n, 1) - getvalue(1,f,t,1,n,0) << '\n';
+        int l, r;
+        cin >> l >> r;
+        l+= bn, r+=bn;
+        int m ,M;
+        m = 0x3f3f3f3f;
+        M = 0;
+
+        while(l<=r){
+            if(l == r){
+                m = MIN(mmin[l], m);
+                M = MAX(mmax[l], M);
+                break;
+            }
+            if(l & 1){
+                m = MIN(mmin[l], m);
+                M = MAX(mmax[l], M);
+            }
+            if(!(r&1)){
+                m = MIN(mmin[r], m);
+                M = MAX(mmax[r], M);
+            }
+            l = (l+1)>>1;
+            r = (r-1) >> 1;
+        }
+        cout << M-m <<'\n';
     }
+
     return 0;
 }
