a937cc97afd33ea7653eb05cd1e8a86f65e6a020
sangdo913 <sangdo913@naver.com>
2020-04-05 17:57:57 +0900

    (36ms, 94.17%) longest-increasing-path-in-a-matrix

--- StudyAlone/StudyAlone/leetcode/longest-increasing-path-in-a-matrix.h
+++ StudyAlone/StudyAlone/leetcode/longest-increasing-path-in-a-matrix.h
@@ -3,23 +3,22 @@ using namespace std;
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 int n, m;
-vector<vector<int>> dp[4];
+vector<vector<int>> dp;
 vector<vector<int>> *arr;
 int dr[4] = {-1,1,0,0};
 int dc[4] = {0,0,-1,1};
 
-int getres(int i, int j, int pd){
-    int &ret = dp[pd][i][j];
-    if(ret != -1) return dp[pd][i][j];
+int getres(int i, int j){
+    int &ret = dp[i][j];
+    if(ret != -1) return dp[i][j];
     ret = 1;
     
     for(int d = 0; d < 4; ++d){
-        if((d^1) == pd) continue;
         int ni = dr[d] + i;
         int nj = dc[d] + j;
         if(ni < 0 || n <= ni || nj < 0 || m <= nj) continue;
         if((*arr)[ni][nj] < (*arr)[i][j]) {
-            int res = getres(ni,nj,d) + 1;
+            int res = getres(ni,nj) + 1;
             if(res > ret) ret = res;
         }
     }
@@ -33,16 +32,16 @@ public:
         arr = &matrix;
         n = matrix.size();
         m = matrix[0].size();
-        for(int i = 0; i < 4; ++i) dp[i] = vector<vector<int>>(n,vector<int>(m, -1));
+        dp = vector<vector<int>>(n,vector<int>(m, -1));
         int res = 1;
+        
         for(int i = 0; i < n; ++i){
             for(int j = 0; j < m; ++j){
-                for(int d = 0; d < 4; ++d){
-                    int r = getres(i,j,d);
-                    if(r > res) res =r;
-                }
+                int r = getres(i,j);
+                if(res < r) res= r;
             }
         }
+        
         return res;
     }
 };
