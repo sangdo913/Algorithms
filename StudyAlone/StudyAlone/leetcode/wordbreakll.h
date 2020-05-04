8d18615bd5a42573965a489d97b2784334b7ceeb
sangdo913 <sangdo913@naver.com>
2020-05-04 21:34:56 +0900

    Word Break II Runtime: 8 ms, faster than 96.48% of C++. Memory Usage: 10.3 MB, less than 96.97% of C++

--- StudyAlone/StudyAlone/leetcode/wordbreakll.h
+++ StudyAlone/StudyAlone/leetcode/wordbreakll.h
@@ -1,47 +1,79 @@
 #include<iostream>
-#include<algorithm>
-#include<string>
-#include<set>
 #include<vector>
-using namespace std;
-vector<int> mvisit;
-vector<vector<string>> res;
-int n;
-set<string> se;
+#include<string>
 
-bool getdp(string &s, vector<string>&wordDict,int i){
-    if(i == n) return true;
-    if(mvisit[i] !=-1){ return mvisit[i];}
-    mvisit[i] = 0;
-    string str;
-    for(int j = i; j < n; ++j){
-        str.push_back(s[j]);
-        if(find(wordDict.begin(), wordDict.end(), str) != wordDict.end()){
-            if(getdp(s,wordDict, j+1)){
-                for(auto it: res[j+1]){
-                    res[i].push_back(str + string(" ") + it);
-                    mvisit[i] = true;
-                }
+using namespace std;
+
+vector<bool> mvisit;
+vector<vector<string>> dp;
+struct NODE{
+    int idx;
+    int child[26];
+    NODE()  {
+        idx = -1;
+        for(int i = 0; i < 26; ++i) child[i] = 0;
+    }
+};
+vector<NODE> nodes;
+int nid = 1;
+int conv[256];
+vector<string> ret;
+
+void getdp(vector<string>& wordDict, string &s, int l){
+    if(l == s.size()){
+       return;
+    }
+    if(mvisit[l]) return;
+    mvisit[l] = 1;
+    
+    int cur = 0;
+    for(int i = l; i < s.size(); ++i){
+        int c = conv[s[i]];
+        if(nodes[cur].child[c] == 0) break;
+        cur = nodes[cur].child[c];
+        if(nodes[cur].idx != -1) {
+            getdp(wordDict, s, i+1);
+            for(auto it : dp[i+1]){
+                dp[l].push_back(wordDict[nodes[cur].idx] + " " + it);
             }
         }
     }
-    return mvisit[i];
 }
 
 class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        se.clear();
+        int cnt = 0;
+        nid = 1;
+        ret.clear();
+        nodes.clear();
+        nodes.resize(100);
         mvisit.clear();
-        mvisit.resize(s.size(),-1);
-        res.clear();
-        n = s.size();
-        res.resize(s.size()+1);
-        res[n].push_back(string(""));
-        sort(wordDict.begin(), wordDict.end());
-        getdp(s,wordDict, 0);
-        for(auto &it : res[0]) it.pop_back();
+        mvisit.resize(s.size(), false);
+        dp.clear();
+        dp.resize(s.size()+1);
+        dp[s.size()].push_back("");
+        for(int i = 'a'; i<='z'; ++i) conv[i] = cnt++;
+        for(int i = 0; i < 26; ++i) nodes[0].child[i] = 0;
+        for(int i = 0; i < wordDict.size(); ++i){
+            int cur = 0;
+            for(int j = 0; j < wordDict[i].size(); ++j){
+                int c = conv[wordDict[i][j]];
+                if(!nodes[cur].child[c]){
+                    if(nid == nodes.size()){
+                        nodes.push_back(NODE());
+                    }
+                    nodes[cur].child[c] = nid++;
+                }
+                cur = nodes[cur].child[c];
+            }
+            nodes[cur].idx = i;
+        }
+        getdp(wordDict,s, 0);
+        for(auto &it : dp[0]){
+            it.pop_back();
+        }
         
-        return res[0];
+        return dp[0];
     }
 };
