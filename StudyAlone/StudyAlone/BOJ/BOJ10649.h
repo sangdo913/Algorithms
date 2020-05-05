17c4eeb25abc3f3847f13157852d9ef765f45dce
sangdo913 <sangdo913@naver.com>
2020-05-05 14:29:37 +0900

    boj 10649 was fixed (more faster)

--- StudyAlone/StudyAlone/BOJ/BOJ10649.h
+++ StudyAlone/StudyAlone/BOJ/BOJ10649.h
@@ -1,45 +1,41 @@
 #include<iostream>
+#include<algorithm>
 #include<cstring>
 
 using namespace std;
-int dp[1<<20];
-
 int info[20][3];
+int idx[20];
 int n,h;
-#define HSIZE 10007
 #define MIN(x,y) ((x) > (y) ? (y) : (x))
 #define MAX(x,y) ((x) > (y) ? (x) : (y))
 enum INFO {L, W, F};
+bool cmp(int a, int b){
+    return info[a][W] + info[a][F] > info[b][W] + info[b][F];
+}
+int res = -1;
 
-int getdp(int visit, int t, long long w){
-    if(dp[visit] != -1) return dp[visit];
+void getdp(int i, int t, int s){
     if(t >= h){
-        return 0x3f3f3f3f;
+        res = MAX(s, res);
+        return;
     }
-    int &ret = dp[visit];
-    ret = -2;
-    for(int i = 0;  i< n; ++i){
-        if(visit & (1<<i)) continue;
-        if(info[i][F] < w) continue;
-        if(w > 1000000000) continue;
-        int res = getdp(visit | (1<<i), t + info[i][L], w + info[i][W]);
-        if(res == -2) continue;
-        res = MIN(res, info[i][F] - w);
-        ret = MAX(ret,res);
-    }
-    return ret;
+    if(i == n) return;
+    int ii = idx[i];
+    if(s >= info[ii][W]) getdp(i+1, t+info[ii][L], MIN(s - info[ii][W], info[ii][F]));
+    getdp(i+1, t, s);
 }
 
 int main(){
-    memset(dp, -1, sizeof(dp));
     freopen("0Text.txt", "r", stdin);
     cin >> n >> h;
     for(int i = 0; i < n; ++i){
+        idx[i] = i;
         cin >> info[i][0] >> info[i][1] >> info[i][2];
     }
-    int ret = getdp(0,0,0);
-    if(ret == -2) cout <<"Mark is too tall\n";
-    else cout << ret <<'\n';
+    sort(idx, idx + n, cmp);
+    getdp(0,0,0x3f3f3f3f);
+    if(res == -1) cout <<"Mark is too tall\n";
+    else cout << res<<'\n';
 
     return 0;
 }
