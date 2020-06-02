dd8a6d2ee15410c7e68df4af7a2595b149339482
sangdo913 <sangdo913@naver.com>
2020-06-02 21:58:49 +0900

    Runtime: 96 ms, faster than 63.51% of C++ online submissions for Decode Ways II. Memory Usage: 11.6 MB, less than 100.00% of C++ online submissions for Decode Ways II.

--- StudyAlone/StudyAlone/leetcode/639. Decode Ways II.h
+++ StudyAlone/StudyAlone/leetcode/639. Decode Ways II.h
@@ -1,62 +1,45 @@
 #include<iostream>
 #include<string>
-#include<vector>
 using namespace std;
 
 #define MOD 1000000007
-string str;
-vector<int> dp;
-int ncnt[11*11];
-int conv[256];
 
-int getdp(int i){
-    if(i == str.size()) return 1;
-    if(dp[i] != -1) {
-        return dp[i];
-    }
-    dp[i] = 0;
-    
-    //한자리
-    if(str[i] == '0') return dp[i] = 0;
-    else if(str[i] == '*') dp[i] = (1LL*getdp(i+1)*9)%MOD;
-    else dp[i] = getdp(i+1);
-    
-    //두자리
-    if(i +1 < str.size()){
-        int c = conv[str[i]]*11 + conv[str[i+1]];
-        dp[i] = (dp[i] + 1LL*ncnt[c]*getdp(i+2))%MOD;
-    }
-    
-    return dp[i];
-}
-
-int getnum(int n[2]){
-    if(n[0] == 0) return 0;
-    else if(n[0] != 10 && n[0] > 2) return 0;
-    else if(n[0] ==2 && n[1] != 10 && n[1] > 6) return 0;
-    else if(n[0] == n[1]  && n[0] == 10) return 15;
-    else if(n[0] == 10) {
-        if(n[1] <= 6) return 2;
-    }  
-    else if(n[1] == 10){
-        if(n[0] == 2) return 6;
-        else return 9;
-    }
-    return 1; 
-}
 class Solution {
 public:
     int numDecodings(string s) {
-        int cnt = 1;
-        for(int i = '0'; i <= '9'; ++i) conv[i] = i-'0';
-        conv['*'] = 10;
-        for(int i = 0; i <= 10; ++i) for(int j = 0; j <= 10; ++j){
-            int n[2] = {i,j};
-            ncnt[i*11+j] = getnum(n);
+        long long prev = 1, pprev = 1;
+        long long res = 1;
+        if(s[0] == '*') prev = 9;
+        else if(s[0] == '0') prev = 0;
+        else prev = 1;
+        for(int i = 1; i < s.size(); ++i){
+            long long nxt = 0;
+            //2개
+            if(s[i-1] == '*'){
+                if(s[i] == '*'){
+                    nxt = pprev*15%MOD;
+                }
+                else if(s[i] <= '6') nxt = 2*pprev%MOD;
+                else nxt = pprev;
+            }
+            else if(s[i-1] == '1'){
+                if(s[i] == '*') nxt = pprev*9%MOD; 
+                else nxt = pprev;
+            }
+            else if(s[i-1] == '2'){
+                if(s[i] == '*') nxt = pprev*6%MOD;
+                else if(s[i] <= '6') nxt = pprev;
+            }
+            //1개
+            if(s[i] == '*'){
+                nxt = (nxt + prev*9)%MOD;
+            }
+            else if(s[i] != '0'){
+                nxt = (nxt +prev)%MOD;
+            }
+            pprev = prev;
+            prev = nxt;
         }
-        str = s;
-        dp.clear();
-        dp.resize(s.size(), -1);
-        return getdp(0);
+        return prev;
     }
 };
